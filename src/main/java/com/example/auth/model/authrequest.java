4bc5326a2a09e182fc1e7054255cde8eee58d301
Hardik <22bcs17077@cuchd.in>
2025-06-26 12:31:15 +0530

    Update authrequest.java

---
+++ src/main/java/com/example/auth/model/authrequest.java
@@ -0,0 +1,15 @@
+package com.example.auth.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class authrequest {
+    private String username;
+    private String password;
+    
+
+}
